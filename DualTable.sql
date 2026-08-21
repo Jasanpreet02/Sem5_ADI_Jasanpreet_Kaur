65432e9b542f4241a050ab1421f83c23e418eb55
Jasanpreet02 <jass2008kaur@gmail.com>
2026-08-21 08:54:26 +0530

    Add files via upload

---
+++ DualTable.sql
@@ -0,0 +1,33 @@
+-- DUAL TABLE
+mysql> SELECT 44+11;
++-------+
+| 44+11 |
++-------+
+|    55 |
++-------+
+1 row in set (0.03 sec)
+
+mysql> SELECT NOW();
++---------------------+
+| NOW()               |
++---------------------+
+| 2026-08-08 12:46:12 |
++---------------------+
+1 row in set (0.00 sec)
+
+mysql> SELECT UCASE('jasan');
++----------------+
+| UCASE('jasan') |
++----------------+
+| JASAN          |
++----------------+
+1 row in set (0.01 sec)
+
+mysql> select lcase('JASAN');
++----------------+
+| lcase('JASAN') |
++----------------+
+| jasan          |
++----------------+
+1 row in set (0.00 sec)
+
